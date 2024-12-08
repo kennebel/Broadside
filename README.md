1ca436b7a3924969eb50c6dda018fd226a760e5c
Jeremy A. Cunningham <jac@kennebel.com>
2024-12-08 13:13:46 -0700

    Added in spawn positions and tried it out by spawning in the placeholder ship.

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Playing around with C# and Godot with a little sci-fi top down ship battle/shooting game.
 
 Might spin off the idea to a VR/XR type thing because i like playing around in that space, who knows. :)
+
+Apache License Version 2.0
--- scripts/world_battle.cs
+++ scripts/world_battle.cs
@@ -1,15 +1,55 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class world_battle : Node3D
 {
+	[Export]
+	public string TestObject { get; set; }
+	[Export]
+	public Node3D SpawnPos0 { get; set; }
+	[Export]
+	public Node3D SpawnPos1 { get; set; }
+
+	private List<Node3D> LoadedObjects { get; set; }
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		LoadedObjects = new List<Node3D>();
+
+		// Temp
+		SpawnIn();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public void SpawnIn()
+	{
+		var scene = GD.Load<PackedScene>("res://objects/"+TestObject+".tscn");
+
+		LoadedObjects.Add(scene.Instantiate<Node3D>());
+		LoadedObjects.Add(scene.Instantiate<Node3D>());
+
+		LoadedObjects[0].Name = "Ship0";
+		LoadedObjects[0].Position = SpawnPos0.Position;
+
+		LoadedObjects[1].Name = "Ship1";
+		LoadedObjects[1].Position = SpawnPos1.Position;
+
+		AddChild(LoadedObjects[0]);
+		AddChild(LoadedObjects[1]);
+	}
+
+	public void CleanUp()
+	{
+		foreach (var Item in LoadedObjects)
+		{
+			RemoveChild(Item);
+		}
+		LoadedObjects.Clear();
 	}
 }
